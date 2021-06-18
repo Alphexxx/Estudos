f8cc6bced6cea77bdeff1c5bf7445b9b2512df82
Leonardo Minichelo <leonardo.mini@live.com>
2021-06-18 09:04:11 -0300

    teste001 adicionado
    adicionado o script do teste001 que estava faltando.

---
+++ Python/scripts-python/testes/teste001.py
@@ -0,0 +1,22 @@
+nome = input('Qual é o seu nome ? ')
+idade = int(input('Qual é a sua idade? '))
+peso = input('Qual é o seu peso? ')
+dia = int(input('Dia = '))
+mês = input('Mês = ')
+ano = int(input('Ano = '))
+
+print('Olá', nome+',', 'é um prazer em te conhecer!','Você tem', idade, 'anos,', 'nasceu no dia', dia, 'de', mês, 'de', ano, 'e pesa', peso, 'Kg.')
+
+n1 = int( input ('Digite um número: '))
+n2 = int( input ('Digite outro número: '))
+resultado = n1+n2
+resultado2 = n1-n2
+resultado3 = n1*n2
+resultado4 = n1/n2
+
+print('O resultado da soma desse dois números é:', resultado)
+print('O resultado da subtração desse dois números é:', resultado2)
+print('O resultado da multiplicação desse dois números é:', resultado3)
+print('O resultado da divisão desse dois números é:', resultado4)
+
+print('====== Final do teste ======')
